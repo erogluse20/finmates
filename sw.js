5216f990b1a8fb300b0c5ba715ae12d24d865434
erogluse20 <168772780+erogluse20@users.noreply.github.com>
2026-01-16 22:20:50 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'finmates-v1';
+const CACHE_NAME = 'finmates-v3';
 const ASSETS_TO_CACHE = [
     './',
     './index.html',
@@ -8,11 +8,11 @@ const ASSETS_TO_CACHE = [
     './css/forms.css',
     './js/app.js',
     './js/components/FeedItem.js',
-    './js/views/CreatePortfolio.js',
-    './assets/icon.png'
+    './js/views/CreatePortfolio.js'
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting(); // Force update
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             return cache.addAll(ASSETS_TO_CACHE);
@@ -20,10 +20,25 @@ self.addEventListener('install', (event) => {
     );
 });
 
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
 self.addEventListener('fetch', (event) => {
+    // Network-first strategy for development
     event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
         })
     );
 });
